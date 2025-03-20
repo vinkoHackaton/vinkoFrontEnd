4af4ebcc0812a137e0129937f4f06ae65286ee91
Marta Ibarra <martaa.ibarra@hotmail.com>
2025-03-20 19:24:56 +0100

    Create routing for pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,17 @@ import "./App.css";
 import CompanionPage from "./pages/CompanionPage/CompanionPage";
 import UserProfilePage from "./pages/UserProfilePage/UserProfilePage";
 import MainPage from "./pages/MainPage/MainPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage/>} />
+        <Route path="/user" element={<UserProfilePage/>} />
+        <Route path="/companion" element={<CompanionPage/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
